1342c7e39cc7dce5dafa1beacdd95daeac6f25f7
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-03-03 23:26:52 +0500

    component: add introduction component

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,21 @@
+function Introduction() {
+  return (
+    <div className='fullsection'>
+      <div>
+        <h1>Ahmad Hassan</h1>
+      </div>
+
+      <div>
+        <h3>Contacts</h3>
+        <div>
+          0307-4458729
+        </div>
+        <div>
+          a2h.ahmadhassan@gmail.com
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Introduction;
